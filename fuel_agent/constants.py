93c25ed2eb40549d3a47e2db874d84ae70b25bb8
Jonathan Zhang <13305198813@163.com>
2026-04-17 22:35:21 +0800

    Add fuel_agent/constants.py

---
+++ fuel_agent/constants.py
@@ -0,0 +1,12 @@
+"""Constants for Fuel Agent Kit."""
+
+# Fuel Network RPC
+FUEL_RPC_URL = "https://beta-4.fuel.network"
+
+# Contract Addresses
+MIRA_CONTRACT = "0x..."
+SWAYLEND_CONTRACT = "0x..."
+
+# Token Addresses
+ETH_TOKEN = "0x..."
+USDC_TOKEN = "0x..."
